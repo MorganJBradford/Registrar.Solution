063734e29c49c26815f80670751b77e945cc883b
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-15 17:11:04 -0500

    add Student/Index route
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

---
+++ Registrar/Controllers/StudentsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic; // to be able to use lists, dictionary
+using System.Linq;// ORM, to be able query objects
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering; //html helpers
+
+namespace Registrar.Controllers
+{
+  public class StudentsController : Controllers
+  {
+private readonly RegistrarContext _db;
+public StudentsController(RegistrarContext db)
+{
+  _db = db;
+}
+
+public ActionResult Index()
+{
+  return View(_db.Students.ToList());
+}
+
+
+}
+}
