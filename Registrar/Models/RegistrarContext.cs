000a8400bdb37ebdb35f7a1816dd288ae2fb4b1a
Morgan Bradford <morganjbradford95@gmail.com>
2021-03-16 17:37:11 -0500

    correct migration pathing
    Co-authored-by: Olha Hizhytska <olgainfotech@gmail.com>
    Co-authored-by: Levi Kohler <kohler.la01@gmail.com>

--- Registrar/Models/RegistrarContext.cs
+++ Registrar/Models/RegistrarContext.cs
@@ -7,7 +7,7 @@ namespace Registrar.Models
     public DbSet<Student> Students { get; set; }
     public DbSet<Course> Courses { get; set; }
     public DbSet<CourseStudent> CourseStudent { get; set; }
-    public DbSet<Department> Department { get; set; }
+    public DbSet<Department> Departments { get; set; }
 
     public RegistrarContext(DbContextOptions options) : base(options) { }
 
